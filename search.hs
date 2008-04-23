cc1a51cd9d489189edc046233be5606e7d5bbac4
Arnar Birgisson <arnarbi@gmail.com>
2008-04-23 23:43:53 +0000

    Uppkast að A*

--- search.hs
+++ search.hs
@@ -22,7 +22,6 @@ queueEmpty :: Queue a -> Bool
 queueEmpty (MkQueue ins outs) = (null ins) && (null outs)
 
 
-
 bfs :: forall a. Int -> (a -> [a]) -> a -> (a -> Bool) -> Maybe [a]
 bfs maxdepth succGenerator start goaltest =
     bfs' $ queuePut [start] mkQueue
@@ -53,6 +52,7 @@ dfs maxdepth succGenerator start goaltest =
             firstJust ((Just x):xs) = Just x
             firstJust (_:xs) = firstJust xs
 
+
 ida :: forall a. Int -> (a -> [a]) -> a -> (a -> Bool) -> Maybe [a]
 ida = ida' 1
       where
@@ -61,3 +61,43 @@ ida = ida' 1
             | otherwise = case dfs d succ s0 goal of
                             Just xs -> Just xs
                             Nothing -> ida' (d+1) maxdepth succ s0 goal
+
+
+
+-- For heuritstic searches, the successor generator function must return
+-- a list of tuples (c,state) where c represents the cost of making the
+-- transition. The last parameter is the heuristic function h, mapping
+-- states to the heuristic estimate. This replaces the goal-test function
+-- as h(s) == 0 implies that s is a goal state.
+-- The type parameter b represents the type of the cost/heuristic, usually
+-- Int or a floating point type.
+
+-- data OpenListElement a b = OpenListElement {
+      
+-- type OpenList a b = (Map.Map
+
+astar :: forall a b. Ord b => Int -> (a -> [(b,a)]) -> a -> (a -> b) -> Maybe [a]
+astar maxdepth succ s0 h =
+    Nothing
+    where
+      astar' :: Int -> OpenList -> ClosedList -> Maybe [a]
+      astar' d open closed | d > maxdepth = Nothing
+                           | emptyOL open = Nohting
+                           | h node == 0  = Just build_solution
+                           | otherwise =
+                               let newnodes = filter (notOnCL closed) (succ node)
+                                   open' = foldl addToOpen open newnodes
+                                   closed' = addToCL closed node
+                               in
+                                 astar' (d+1) open' closed'
+                               where
+                                 addToOpen :: OpenList -> Node -> OpenList
+                                 addToOpen open node =
+                                     if onOL open node then
+                                         if node is better then
+                                             addNode (removeNode open oldnode) node
+                                         else
+                                             open
+                                     else
+                                         addNode open node
+
